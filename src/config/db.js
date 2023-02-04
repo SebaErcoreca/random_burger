f6648d5c7afa49953c301d142ee617842e33b143
ERCORECA Sebastian <ercoreca01@gmail.com>
2023-02-04 14:47:36 -0300

    Solved error in connection

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,17 @@ dotenv.config();
 
 mongoose.set('strictQuery', false)
 
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+  console.log('Database Connection Established ✅');
+}).catch((err)=>{
+  console.log('Database Not Connected ' + err)
+})
+
+/* 
 mongoose.connect(process.env.MONGO_URI, (err) => {
     if (err) {
       console.log("Error:", err);
     } else {
-      console.log('Conection to DB established ✅');
+      console.log('Connection to DB established ✅');
     }
-});
+}); */
